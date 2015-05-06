b7e2fea31c870c46b516c9128552f2374965cae0
Eder Carlos <eder@innovateti.com.br>
2015-05-06 17:12:47 -0300

    Kickoff project using Yeaoman to scafold project

---
+++ app/scripts/app.js
@@ -0,0 +1,33 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name geoCtfApp
+ * @description
+ * # geoCtfApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('geoCtfApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
